b7b9059d0bfaa228f3a6f7873110d8b5243ef78d
Tim Hopgood <thopgood@student.42lisboa.com>
2025-12-01 14:54:38 +0000

    rsg

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -5,6 +5,7 @@ cd /var/www/html
 WP_ADMIN_USER=$(cat ${WP_ADMIN_USER_FILE:-/run/secrets/wp_admin_user})
 WP_ADMIN_PASSWORD=$(cat ${WP_ADMIN_PASSWORD_FILE:-/run/secrets/wp_admin_password})
 DB_PASSWORD=$(cat ${DB_PASSWORD_FILE:-/run/secrets/db_password})
+WP_USER2_PASSWORD=$(cat ${WP_USER2_PASSWORD_FILE:-/run/secrets/wp_user2_password})
 
 # Only initialize if WordPress isn't already installed
 if [ ! -f "wp-load.php" ]; then
@@ -15,7 +16,7 @@ if [ ! -f "wp-load.php" ]; then
     ./wp-cli.phar core install --url=localhost --title="${WP_TITLE}" --admin_user="${WP_ADMIN_USER}" --admin_password="${WP_ADMIN_PASSWORD}" --admin_email="${WP_ADMIN_EMAIL}" --allow-root
     ./wp-cli.phar theme activate astra --allow-root
     ./wp-cli.phar plugin install elementor --activate --allow-root
-    ./wp-cli.phar user create user2 user2@inception.42.fr --user_pass=user2pass --role=subscriber --allow-root
+    ./wp-cli.phar user create user2 user2@inception.42.fr --user_pass="${WP_USER2_PASSWORD}" --role=subscriber --allow-root
 fi
 
 # Run php-fpm in foreground and exit with its exit code
